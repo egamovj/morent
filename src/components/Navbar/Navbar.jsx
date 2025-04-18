911f9425e4135ec9a8f0b538fa3330f95fd8f184
egamovj <egamovj90@gmail.com>
2025-04-18 20:47:50 +0500

    navbar ui completed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,16 +3,35 @@ import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="left">
-        <img src="../../public/icons/Logo.svg" alt="Morent Logo" />
-        <div className="seacrh">
+    <div className="container">
+      <nav>
+        <div className="left">
+          <img src="../../public/icons/Logo.svg" alt="Morent Logo" />
+          <div className="search">
             <img src="../../public/icons/search-normal.svg" alt="Search icon" />
             <input type="search" placeholder="Search something here" />
+            <img src="../../public/icons/filter.svg" alt="Filter icon" />
+          </div>
         </div>
-      </div>
-      <div className="right"></div>
-    </nav>
+        <div className="right">
+          <div className="heart">
+            <img src="../../public/icons/heart.svg" alt="Heart icon" />
+          </div>
+          <div className="not-ion">
+            <img
+              src="../../public/icons/notification.svg"
+              alt="Notification icon"
+            />
+          </div>
+          <div className="setting">
+            <img src="../../public/icons/setting-2.svg" alt="Setting icon" />
+          </div>
+          <div className="profile">
+            <img src="../../public/images/Profil.png" alt="Profil picture" />
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
