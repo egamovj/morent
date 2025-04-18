e8bd29f46b28442d9a08a486208da048834f7dd9
egamovj <egamovj90@gmail.com>
2025-04-18 20:53:09 +0500

    created hero section component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
   return (
     <>
       <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
