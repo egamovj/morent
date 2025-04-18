007317512a3b476cb67fd9f04d58d20013594346
egamovj <egamovj90@gmail.com>
2025-04-18 21:43:32 +0500

    start popular section ui

---
+++ src/components/Popular/Popular.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./popular.css";
+
+const Popular = () => {
+  return (
+    <div className="popular">
+      <div className="container">
+        <section className="popular-sec">
+            
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Popular;
