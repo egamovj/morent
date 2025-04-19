9221f115115e7c9b3e159029e12d96297d3a3c92
egamovj <egamovj90@gmail.com>
2025-04-19 20:54:19 +0500

    starting new section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Popular from "./components/Popular/Popular";
+import Recomendation from "./components/Recomendation/Recomendation";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Popular />
+      <Recomendation />
     </>
   );
 }
---
+++ src/components/Recomendation/Recomendation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './recomendation.css'
+
+const Recomendation = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Recomendation
